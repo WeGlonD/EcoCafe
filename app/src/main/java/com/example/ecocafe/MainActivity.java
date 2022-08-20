e92747d92a1dc19f8983867411f7cb2321cf8d7d
이동원 <retomoo@naver.com>
2022-08-20 16:13:55 +0900

    바텀 내비게이션바 추가

--- app/src/main/java/com/example/ecocafe/MainActivity.java
+++ app/src/main/java/com/example/ecocafe/MainActivity.java
@@ -4,11 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView main_bottom = findViewById(R.id.main_bottom);
+        BottomNavigationHelper.disableShiftMode(main_bottom);
+/*
+        homeFragment = new HomeFragment();
+        listFragment = new ListFragment();
+        myPageFragment = new MyPageFragment();
+        mapFragment = new MapFragment();
+ */
+
     }
+/*
+    public void setFragment(int n){
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        switch (n){
+            case 0:
+                fragmentTransaction.replace(R.id.main_frame,homeFragment).commit();
+                break;
+            case 1:
+                fragmentTransaction.replace(R.id.main_frame, hotCommunityFragment).commit();
+                break;
+            case 2:
+                fragmentTransaction.replace(R.id.main_frame,myPageFragment).commit();
+                break;
+            case 3:
+                fragmentTransaction.replace(R.id.main_frame,alarmFragment).commit();
+                break;
+        }
+*/
 }
