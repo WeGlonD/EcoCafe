f39c285453ce906b3f32ddef137ff0c4a7362d70
이동원 <retomoo@naver.com>
2022-08-23 18:22:21 +0900

    네비게이션 탭 전환 기초 설계

--- app/src/main/java/com/example/ecocafe/MainActivity.java
+++ app/src/main/java/com/example/ecocafe/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.ecocafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    MapTab mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,31 +25,39 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView main_bottom = findViewById(R.id.main_bottom);
         BottomNavigationHelper.disableShiftMode(main_bottom);
-/*
-        homeFragment = new HomeFragment();
-        listFragment = new ListFragment();
-        myPageFragment = new MyPageFragment();
-        mapFragment = new MapFragment();
- */
+
+        //homeFragment = new HomeFragment();
+        //listFragment = new ListFragment();
+        //myPageFragment = new MyPageFragment();
+        mapFragment = new MapTab();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.main_bottom);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                setFragment(item.getItemId());
+                return true;
+            }
+        });
 
     }
-/*
-    public void setFragment(int n){
+
+    public void setFragment(int n) {
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
-        switch (n){
+        switch (n) {
             case 0:
-                fragmentTransaction.replace(R.id.main_frame,homeFragment).commit();
+                //fragmentTransaction.replace(R.id.main_frame,homeFragment).commit();
                 break;
-            case 1:
-                fragmentTransaction.replace(R.id.main_frame, hotCommunityFragment).commit();
+            case R.id.bottom_map:
+                fragmentTransaction.replace(R.id.main_frame, mapFragment).commit();
                 break;
             case 2:
-                fragmentTransaction.replace(R.id.main_frame,myPageFragment).commit();
+                //fragmentTransaction.replace(R.id.main_frame,myPageFragment).commit();
                 break;
             case 3:
-                fragmentTransaction.replace(R.id.main_frame,alarmFragment).commit();
+                //fragmentTransaction.replace(R.id.main_frame,alarmFragment).commit();
                 break;
         }
-*/
+    }
 }
